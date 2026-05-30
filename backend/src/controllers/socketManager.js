c37bc94009e197d4214a1f3a3230cc60b552d2b4
bhoomi chhatbar <bhoomichhatbar@gmail.com>
2026-05-30 15:39:22 +0530

    Add production frontend URL to socket cors

--- backend/src/controllers/socketManager.js
+++ backend/src/controllers/socketManager.js
@@ -8,7 +8,10 @@ const connectToSocket = (server) => {
 
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: [
+      "http://localhost:5173",
+      "https://talkflow-frontend-qs09.onrender.com",
+    ],
     },
   });
 
